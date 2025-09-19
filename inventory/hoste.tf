22bd4fefcbf92c5f18b7f1b61fde38255da92609
Martin Koldovský @ CHKP <73023844+chkp-mkoldov@users.noreply.github.com>
2025-09-19 05:34:43 +0000

    bug2 manicka

--- inventory/hoste.tf
+++ inventory/hoste.tf
@@ -5,6 +5,6 @@ resource "checkpoint_management_host" "manicka2" {
   ipv4_address = "192.168.1.4"
 
   color = "pink"
-  tags  = concat(each.value.tags, ["terraform", "MadeByTF"])
+  tags  =  ["terraform", "MadeByTF"]
 
 }
