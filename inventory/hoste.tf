db4d58bc9dde0c953910702c852d5b81634918ec
Martin Koldovský @ CHKP <73023844+chkp-mkoldov@users.noreply.github.com>
2025-09-19 05:40:17 +0000

    m3

--- inventory/hoste.tf
+++ inventory/hoste.tf
@@ -8,3 +8,14 @@ resource "checkpoint_management_host" "manicka2" {
   tags  =  ["terraform", "MadeByTF"]
 
 }
+
+resource "checkpoint_management_host" "manicka3" {
+
+
+  name         = "manicka3"
+  ipv4_address = "192.168.1.4"
+
+  color = "pink"
+  tags  =  ["terraform", "MadeByTF"]
+
+}
